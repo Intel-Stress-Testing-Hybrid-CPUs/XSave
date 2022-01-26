bab260718ec6446c00c8e982f0b88ded42cc58a4
viraj <viraj.wadhwa@utexas.edu>
2022-01-26 13:28:29 -0700

    added some testing for saving and restoring state for XSAVE

--- XsaveProject/XSAVE.cpp
+++ XsaveProject/XSAVE.cpp
@@ -6,7 +6,7 @@
 #include <string> // for to_string() method to go from int to string
 #include <stdlib.h>
 #include <stdio.h>
-#include <memory>   
+#include <memory> // for void *align
 #include <exception>
 #include "FP_loop_test.h"
 #include "InstructionSet.h" // for seeing which Instructions are supported/enabled
@@ -24,38 +24,63 @@ using namespace std;
 int total_bytes = 512;
 int byte_boundary_size = 64;
 
-    int main(int argc, char *argv[]) {
+// print out the entire xsave data state region
+void print_xsave(int* xsavedata_int) {
+    for (int i = 0; i < (total_bytes / 4); i++) {
+        int last_byte = (i + 1) * 4 - 1;
+        int first_byte = (i + 1) * 4 - 4;
+        cout << "byte " << to_string(first_byte) << "-" << to_string(last_byte) << ": " << to_string(xsavedata_int[i]) << endl;
+    }
+}
 
-        /*
-        Allocate 128 * 4 = 512 bytes aligned on a 64 byte boundary for XSAVE. This differs from FXSAVE which requires 16 byte boundary
-        https://www.felixcloutier.com/x86/xsave
-        */
+void change_xsave_data_random(int* xsavedata_int) {
+    for (int i = 0; i < (total_bytes / 4); i++) {
+        xsavedata_int[i] = rand();
+    }
+}
 
-        // allocate the xsave data region given by 'total_bytes' on a 'byte_boundary_size' boundary
-        void* xsavedata = _aligned_malloc(total_bytes, byte_boundary_size);
+int main(int argc, char *argv[]) {
 
-        // clear the data inside the region to all 0s
-        memset(xsavedata, 0, total_bytes);
+    /*
+    Allocate 128 * 4 = 512 bytes aligned on a 64 byte boundary for XSAVE. This differs from FXSAVE which requires 16 byte boundary
+    https://www.felixcloutier.com/x86/xsave
+    */
 
-        // check if XSAVE is supported either by CPU or OS
-        // uses InstructionSet.cpp
-        check_instructions();
+    // allocate the xsave data region given by 'total_bytes' on a 'byte_boundary_size' boundary
+    void* xsavedata = _aligned_malloc(total_bytes, byte_boundary_size);
+    if (xsavedata == nullptr) {
+        throw exception("Aligned malloc failed, returned nullptr");
+    }
+
+    // clear the data inside the region to all 0s
+    memset(xsavedata, 0, total_bytes);
+
+    // check if XSAVE is supported either by CPU or OS
+    // uses InstructionSet.cpp
+    check_instructions();
+
+    while (true) {
 
         // call xsave and save the processor state (note this is 32 bit mode)
         // saves everything in processor state that can be saved
-         _xsave(xsavedata, 0xFu);
+        _xsave(xsavedata, 0xFu);
 
-         // print out the results
-         int* xsavedata_int = (int*)xsavedata;
-         for (int i = 0; i < (total_bytes / 4); i++) {
-             int last_byte = (i + 1) * 4 - 1;
-             int first_byte = (i + 1) * 4 - 4;
-             cout << "byte " << to_string(first_byte) << "-" << to_string(last_byte) << ": " << to_string(xsavedata_int[i]) << endl;
-         }
-        
-         // free allocated data region using special _aligned_free() method
-         // TODO: fix _aligned_free(xsavedata)
-        _aligned_free(xsavedata);
+        // change the data
+        change_xsave_data_random((int*)xsavedata);
 
-        return 0;
+        // realign the memory region
+        std::size_t space = 512;
+        xsavedata = align(64, 512, xsavedata, space);
+
+        _xrstor(xsavedata, 0xFu);
     }
+
+    // print out the results
+    print_xsave((int*)xsavedata);
+    
+    // free allocated data region using special _aligned_free() method
+    // TODO: fix _aligned_free(xsavedata)
+    _aligned_free(xsavedata);
+
+    return 0;
+}
